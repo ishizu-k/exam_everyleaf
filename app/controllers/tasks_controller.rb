75f0b4c9fb647b476e3e8104f9d183d6d98ad5e4
ishizu <demekinn_96@yahoo.co.jp>
2019-06-25 13:41:18 +0900

    add: 作成画面（投稿フォーム）を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
 end
