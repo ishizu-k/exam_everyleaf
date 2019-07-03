eabe47d5d655444ca5a8a331850ed651eeb1961a
ishizu <demekinn_96@yahoo.co.jp>
2019-07-03 11:32:06 +0900

    add: 終了期限、ステータスにバリデーション（存在性）を設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length:  { maximum: 30 }
   validates :content, presence: true, length: { maximum: 500 }
+  validates :limit, presence: true
+  validates :status, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_061705) do
+ActiveRecord::Schema.define(version: 2019_07_02_071623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_061705) do
     t.string "content", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "limit"
+    t.date "limit", null: false
     t.string "status", default: "", null: false
   end
 
