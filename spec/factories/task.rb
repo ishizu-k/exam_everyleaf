7eabddc7c734b0a52925c1e7b310cca4bceb4129
ishizu <demekinn_96@yahoo.co.jp>
2019-07-08 19:21:33 +0900

    add: 優先順位のソートに関するテストを追加

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
-  # 作成するテストデータの名前を「task」とします
-  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
     name { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
     limit { '2022-08-31'}
     status { '未着手' }
+    priority { 0 }
   end
 
-  # 作成するテストデータの名前を「second_task」とします
-  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
   factory :second_task, class: Task do
     name { 'Factoryで作ったデフォルトのタイトル２' }
     content { 'Factoryで作ったデフォルトのコンテント２' }
     limit { '2019-08-31'}
     status { '着手' }
+    priority { 1 }
   end
 end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -22,19 +22,19 @@ RSpec.feature "タスク管理機能", type: :feature do
     fill_in 'タスク名', with: 'namenamename'
     fill_in 'タスク詳細', with: 'contentcontentcontent'
     fill_in '終了期限', with: '2019-07-07'
-    #save_and_open_page
     select '未着手', from: 'ステータス'
+    select "高", from: '優先順位'
     click_button '新規作成'
     expect(page).to have_content 'namenamename'
     expect(page).to have_content 'contentcontentcontent'
     expect(page).to have_content '2019-07-07'
     expect(page).to have_content '未着手'
+    expect(page).to have_content 0
   end
 
   scenario "タスク詳細のテスト" do
     visit tasks_path
-    #save_and_open_page
-    page.all("td")[4].click_link '詳細'
+    page.all("td")[5].click_link '詳細'
     expect(page).to have_content 'Factoryで作ったデフォルトのタイトル２'
     expect(page).to have_content '付け加えたコンテント'
   end
@@ -61,6 +61,7 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
     expect(page).to have_content '2022-08-31'
     expect(page).to have_content '未着手'
+    expect(page).to have_content 0
   end
 
   scenario "ステータスで検索できるかのテスト" do
@@ -72,5 +73,13 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
     expect(page).to have_content '2022-08-31'
     expect(page).to have_content '未着手'
+    expect(page).to have_content 0
+  end
+
+  scenario "優先順位でソートするとタスクが優先順位の高い順に並び替えられるかのテスト" do
+    visit tasks_path
+    expect(Task.all.order(created_at: :desc)).to eq [@task2, @task1]
+    click_link '終了期限でソートする'
+    expect(Task.all.order(priority: :asc)).to eq [@task1, @task2]
   end
 end
