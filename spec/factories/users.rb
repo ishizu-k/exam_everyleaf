a456fd40ce3f7a8b5d8371e1093cdd4e0f71ffce
ishizu <demekinn_96@yahoo.co.jp>
2019-07-13 15:25:38 +0900

    add: ログインのテストを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :user do
     name { "tanaka" }
     email { "tanaka@example.com" }
-    password { "111111" }
-    password_digest { "111111" }
+    password {"111111"}
+    password_confirmation { "111111" }
   end
 
   factory :second_user, class: User do
     name { "yamada" }
     email { "yamada@example.com" }
     password { "222222" }
-    password_digest { "222222" }
+    password_confirmation { "222222" }
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -16,6 +16,15 @@ RSpec.feature "ユーザー管理機能", type: :feature do
     expect(page).to have_content 'name'
     expect(page).to have_content 'name@example.com'
   end
+
+  scenario "ログインする" do
+    visit new_session_path
+    fill_in 'メールアドレス', with: 'tanaka@example.com'
+    fill_in 'パスワード', with: '111111'
+    click_button 'Log in'
+    expect(page).to have_content 'tanaka'
+    expect(page).to have_content 'tanaka@example.com'
+  end
 end
 
 #テスト
