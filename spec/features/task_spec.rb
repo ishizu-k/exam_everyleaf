46d3cc79dd2a1711fc650fb9af498075802ed519
ishizu <demekinn_96@yahoo.co.jp>
2019-07-16 01:10:31 +0900

    fix: featurespec（タスク管理機能のテスト）の修正

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -4,17 +4,19 @@ require 'rails_helper'
 RSpec.feature "タスク管理機能", type: :feature do
   background do
     # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    # backgroundの中に記載された記述は、そのカテゴリ内（feature "タスク管理機能", type: :feature do から endまでの内部）
-    # に存在する全ての処理内（scenario内）で実行される
-    # （「タスク一覧のテスト」でも「タスクが作成日時の降順に並んでいるかのテスト」でも、background内のコードが実行される）
     @task1 = FactoryBot.create(:task, name: '付け加えた名前')
     @task2 = FactoryBot.create(:second_task, content: '付け加えたコンテント')
+    # ログインする
+    visit new_session_path
+    fill_in 'メールアドレス', with: 'tanaka@example.com'
+    fill_in 'パスワード', with: '111111'
+    click_button 'Log in'
   end
 
   scenario "タスク一覧のテスト" do
     visit tasks_path
     expect(page).to have_content '付け加えた名前'
-    expect(page).to have_content '付け加えたコンテント'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスク作成のテスト" do
@@ -47,16 +49,15 @@ RSpec.feature "タスク管理機能", type: :feature do
   scenario "終了期限でソートするとタスクが終了期限の降順に並び替えられるかのテスト" do
     visit tasks_path
     expect(Task.all.order(created_at: :desc)).to eq [@task2, @task1]
-    click_link '終了期限でソートする'
+    click_link '終了期限'
     expect(Task.all.order(limit: :desc)).to eq [@task1, @task2]
   end
 
   scenario "タスク名で検索できるかのテスト" do
     visit tasks_path
     fill_in 'task_name', with: '付け加えた名前'
-    select '', from: 'task_status'
-    #save_and_open_page
-    click_button '検索'
+    select 'ステータスから検索', from: 'task_status'
+    click_button 'search'
     expect(page).to have_content '付け加えた名前'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
     expect(page).to have_content '2022-08-31'
@@ -68,7 +69,7 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit tasks_path
     fill_in 'task_name', with: ''
     select '未着手', from: 'task_status'
-    click_button '検索'
+    click_button 'search'
     expect(page).to have_content '付け加えた名前'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
     expect(page).to have_content '2022-08-31'
@@ -79,10 +80,11 @@ RSpec.feature "タスク管理機能", type: :feature do
   scenario "優先順位でソートするとタスクが優先順位の高い順に並び替えられるかのテスト" do
     visit tasks_path
     expect(Task.all.order(created_at: :desc)).to eq [@task2, @task1]
-    click_link '終了期限でソートする'
+    click_link '終了期限'
     expect(Task.all.order(priority: :asc)).to eq [@task1, @task2]
   end
 end
 
-#テスト
-#bin/rspec spec/features/task_spec.rb
+# テスト
+# bin/rspec spec/features/task_spec.rb
+# save_and_open_page
