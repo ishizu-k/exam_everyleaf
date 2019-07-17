acbc83e6ffb931e5b7e434bbf77335d18da35e33
ishizu <demekinn_96@yahoo.co.jp>
2019-07-17 10:48:18 +0900

    add: 管理画面にユーザー一覧、ユーザー新規作成、ユーザー編集、ユーザー削除、ユーザーごとのタスク一覧を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,29 +1,51 @@
 class Admin::UsersController < ApplicationController
+  before_action :user_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
-    redirect_to user_path(current_user.id) if logged_in
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to user_path(@user.id)
+      redirect_to admin_users_path
+      flash[:success] = "新規作成しました"
     else
       render 'new'
     end
   end
 
   def show
-    @user = User.find(params[:id])
-    if current_user != @user
-      redirect_to user_path(current_user.id)
+    @tasks = @user.tasks
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:success] = "編集しました"
+    else
+      render 'edit'
     end
   end
 
-  private
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+    flash[:success] = "削除しました"
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_task
+    @user = User.find(params[:id])
   end
 end
