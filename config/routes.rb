ddedf55a6747df0925324fd70c29924e991f0b9d
ishizu <demekinn_96@yahoo.co.jp>
2019-07-19 20:34:10 +0900

    add: labelsコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only:[:new, :create, :show]
   resources :sessions, only:[:new, :create, :destroy]
+  resources :labels, only:[:new, :create, :edit, :destroy]
 end
