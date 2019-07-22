366854d5c792e7f12c88eefa906b6d3b94745580
ishizu <demekinn_96@yahoo.co.jp>
2019-07-22 21:52:07 +0900

    add: features/task_spec"タスクに複数のラベルをつけられる"テストを作成

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 
 RSpec.feature "タスク管理機能", type: :feature do
   background do
-    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    # タスク作成
     @task1 = FactoryBot.create(:task, name: '付け加えた名前')
     @task2 = FactoryBot.create(:second_task, content: '付け加えたコンテント')
+    # ラベル作成
+    @label1 = FactoryBot.create(:label)
+    @label2 = FactoryBot.create(:label, name: "ラベル2")
+    @label3 = FactoryBot.create(:label, name: "ラベル3")
     # ログインする
     visit new_session_path
     fill_in 'メールアドレス', with: 'tanaka@example.com'
@@ -82,6 +86,26 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(Task.all.order(created_at: :desc)).to eq [@task2, @task1]
     click_link '終了期限'
     expect(Task.all.order(priority: :asc)).to eq [@task1, @task2]
+  end
+
+  scenario "タスクに複数のラベルをつけられる" do
+    visit new_task_path
+    fill_in 'タスク名', with: 'name'
+    fill_in 'タスク詳細', with: 'content'
+    fill_in '終了期限', with: '2019-08-08'
+    select '未着手', from: 'ステータス'
+    select "高", from: '優先順位'
+    check 'task_label_ids_25'
+    check 'task_label_ids_27'
+    click_button '新規作成'
+    save_and_open_page
+    expect(page).to have_content 'name'
+    expect(page).to have_content 'content'
+    expect(page).to have_content '2019-08-08'
+    expect(page).to have_content '未着手'
+    expect(page).to have_content 0
+    expect(page).to have_content 'ラベル1'
+    expect(page).to have_content 'ラベル3'
   end
 end
 
