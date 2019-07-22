594ec9739cc1801d1c60cd80f04853e875d1fafe
ishizu <demekinn_96@yahoo.co.jp>
2019-07-22 20:31:12 +0900

    fix: features/user_spec「ユーザーごとに作成したタスク一覧が表示される」テストを修正

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -169,6 +169,12 @@ RSpec.feature "ユーザー管理機能", type: :feature do
     end
 
     scenario "ユーザーごとに作成したタスク一覧が表示される" do
+      # ログインする
+      visit new_session_path
+      fill_in 'メールアドレス', with: 'tanaka@example.com'
+      fill_in 'パスワード', with: '111111'
+      click_button 'Log in'
+      # 管理画面
       visit admin_users_path
       page.all("td")[2].click_link '詳細'
       expect(page).to have_content 'tanakaさんのタスク一覧'
